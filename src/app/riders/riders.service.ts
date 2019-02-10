11382a7e222fbead40eed21138566c9a1f58adb6
swedkg <dimitrios.giannopoulos@gmail.com>
2019-02-10 19:02:39 +0100

    switch to local riders json

--- src/app/riders/riders.service.ts
+++ src/app/riders/riders.service.ts
@@ -10,7 +10,8 @@ export class RidersService {
   constructor(private http: HttpClient) {}
 
   getriders(): Observable<rider> {
-    let url: string = 'https://api.myjson.com/bins/1753m4';
+    // let url: string = 'https://api.myjson.com/bins/1753m4';
+    let url: string = '../../assets/riders.json';
     return this.http.get<rider>(url);
   }
 }
